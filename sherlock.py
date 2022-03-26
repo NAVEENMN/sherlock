06d5ffa594822b63e022eadd45399b75985ded54
Naveen Mysore <navimn1991@gmail.com>
2022-03-26 14:22:47 -0700

    clean up

--- sherlock.py
+++ sherlock.py
@@ -1,6 +1,10 @@
+"""
+Version 1.0
+Sherlock is an investigation tool build to automate investigation of something.
+Sherlock organizes evidences, assumption, validations, references etc solve an investigation.
+Sherlock 1.0 is designed for small scale investigation with small number of hypothesis and evidences.
+"""
 import os
-import shutil
-import subprocess
 from PyInquirer import prompt
 
 question_1 = [
@@ -19,70 +23,19 @@ def setup_directory_structure():
     if not os.path.exists(os.path.join(root_path, 'archive')):
         os.mkdir(os.path.join(root_path, 'archive'))
 
-def open_a_case():
-    print("Opening a case")
-
-    questions_5 = [
-        {
-            'type': 'list',
-            'name': 'case_type',
-            'message': 'What cases you are looking for?',
-            'choices': ['active', 'archived'],
-            'filter': lambda val: val.lower()
-        }
-    ]
-    answers_5 = prompt(questions_5)
-
-    cases = os.listdir(os.path.join(root_path))
-    if '.DS_Store' in cases:
-        cases.remove('.DS_Store')
-    if 'archive' in cases:
-        cases.remove('archive')
-
-    if answers_5 == 'archived':
-        cases = os.listdir(os.path.join(root_path, 'archived'))
-
-    questions_6 = [
-        {
-            'type': 'list',
-            'name': 'case_name',
-            'message': 'Pick a case',
-            'choices': cases,
-            'filter': lambda val: val.lower()
-        }
-    ]
-    answers_6 = prompt(questions_6)
-
-    path = os.path.join(root_path, answers_6['case_name'])
-    if answers_5 == 'archived':
-        path = os.path.join(root_path, 'archived', answers_6['case_name'])
-
-    subprocess.run(["/Applications/Obsidian.app/Contents/MacOS/Obsidian", path])
-
-
-def close_a_case():
-    print(f"Closing a case")
-    active_cases = os.listdir(os.path.join(root_path))
-    if '.DS_Store' in active_cases:
-        active_cases.remove('.DS_Store')
-    if 'archive' in active_cases:
-        active_cases.remove('archive')
-    questions_4 = [
-        {
-            'type': 'list',
-            'name': 'case_name',
-            'message': 'What is the aim of research?',
-            'choices': active_cases,
-            'filter': lambda val: val.lower()
-        }
-    ]
-    answers_4 = prompt(questions_4)
-    shutil.move(src=os.path.join(root_path, answers_4['case_name']), dst=os.path.join(root_path, 'archive'))
-    print(f"Case {answers_4['case_name']} archived.")
-
 
 def create_a_new_case():
     print(f"Creating a new case")
+
+    questions_4 = [
+        {
+            'type': 'input',
+            'name': 'directory_name',
+            'message': 'Project name: ',
+        }
+    ]
+    answers_4 = prompt(questions_4)
+
     questions_3 = [
         {
             'type': 'input',
@@ -91,79 +44,69 @@ def create_a_new_case():
         }
     ]
     answers_3 = prompt(questions_3)
-    archived_cases = os.listdir(os.path.join(root_path, 'archive'))
-    active_cases = os.listdir(os.path.join(root_path))
-    if '.DS_Store' in archived_cases:
-        archived_cases.remove('.DS_Store')
-    if '.DS_Store' in active_cases:
-        active_cases.remove('.DS_Store')
-    if 'archive' in active_cases:
-        active_cases.remove('archive')
 
-    case_cnt = len(archived_cases) + len(active_cases)
-    new_case_name = f"case_{case_cnt+1}"
+    new_case_dir = f"case_{answers_4['directory_name']}"
 
-    if not os.path.exists(os.path.join(root_path, new_case_name)):
-        os.mkdir(os.path.join(root_path, new_case_name))
-        os.mkdir(os.path.join(root_path, new_case_name, 'media'))
+    if not os.path.exists(os.path.join(root_path, new_case_dir)):
+        os.mkdir(os.path.join(root_path, new_case_dir))
+        os.mkdir(os.path.join(root_path, new_case_dir, 'media'))
+        os.mkdir(os.path.join(root_path, new_case_dir, 'evidences'))
 
-    with open(os.path.join(root_path, new_case_name, 'main.md'), "w") as fp:
+    with open(os.path.join(root_path, new_case_dir, 'notes.md'), "w") as fp:
+        fp.write(f"# Notes\n")
+        fp.write("---\n")
+
+    with open(os.path.join(root_path, new_case_dir, 'main.md'), "w") as fp:
         fp.write(f"# {answers_3['title']}\n")
         fp.write("---\n")
         fp.write("[[references]] [[evidences]]\n")
 
         fp.write("## Description\n")
-        fp.write("Case description goes here.\n")
+        fp.write("Case description goes here. A short paragraph summarizing what has happend? "
+                 "This a living document until the investigation closes.\n")
+
+        fp.write("## Chain of events\n")
+        fp.write("A list of events indexed by time. \n")
+        fp.write("| Time      | Event |\n")
+        fp.write("| ----------- | ----------- |\n")
+        fp.write("| Jan 1      | Event 1 Occurs       |\n")
+        fp.write("* Additional details for chain of events\n")
 
         fp.write("## Hypothesis\n")
-        fp.write("Hypothesis 1\n")
-        fp.write("Link to evidences in favour: \n")
-        fp.write("Link to evidences not in favour: \n")
-        fp.write("Hypothesis 2\n")
-        fp.write("Link to evidences in favour: \n")
-        fp.write("Link to evidences not in favour: \n")
+        fp.write("A list of hypothesis that explains the observation. \n")
+        fp.write("| Hypothesis      | Description |\n")
+        fp.write("| ----------- | ----------- |\n")
+        fp.write("| Hypothesis 1| A is causing B       |\n")
+        fp.write("* Additional details for chain of events\n")
+
+        fp.write("## Hypothesis Testing\n")
+        fp.write("A Hypothesis can be tested. It can be backed by theoritical or experimental evidence. \n")
+
+        fp.write("| Hypothesis  | Status | Evidence | \n")
+        fp.write("| ----------- | ----------- |----------- |\n")
+        fp.write("| Hypothesis 1| Not tested(T/F) | links |\n")
 
         fp.write("## Conclusion\n")
         fp.write("Your conclusion goes here.\n")
 
-    with open(os.path.join(root_path, new_case_name, 'references.md'), "w") as fp:
+    with open(os.path.join(root_path, new_case_dir, 'references.md'), "w") as fp:
         fp.write(f"# References \n")
         fp.write("---\n")
 
-    os.mkdir(os.path.join(root_path, new_case_name, 'evidences'))
-    with open(os.path.join(root_path, new_case_name, 'evidences', 'evidence_copy.md'), "w") as fp:
-        fp.write(f"# Evidence Copy\n")
+    with open(os.path.join(root_path, new_case_dir, 'evidences', 'evidence_1.md'), "w") as fp:
+        fp.write(f"# Evidence name\n")
         fp.write("---\n")
         fp.write("A is correlated with B\n")
         fp.write("Here is a plot\n")
 
-    os.mkdir(os.path.join(root_path, new_case_name, 'code'))
+    os.mkdir(os.path.join(root_path, new_case_dir, 'code'))
 
-    print(f"case {new_case_name} created.")
+    print(f"case {answers_3['title']} created.")
 
 
 def main():
     setup_directory_structure()
-
-    question_2 = [
-        {
-            'type': 'list',
-            'name': 'options',
-            'message': 'Please select the tool you wish to use.\n',
-            'choices': ['open a case', 'create new case', 'close a case'],
-        }
-    ]
-    answers_2 = prompt(question_2)
-    opts = answers_2['options']
-
-    if opts == "create new case":
-        create_a_new_case()
-
-    if opts == "close a case":
-        close_a_case()
-
-    if opts == "open a case":
-        open_a_case()
+    create_a_new_case()
 
 
 if __name__ == "__main__":
